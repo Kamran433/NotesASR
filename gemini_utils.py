22dfd189c81fdab593009f53f1053999fb4997c5
Kamran Ali <102954239+Kamran433@users.noreply.github.com>
2025-05-11 01:07:45 +0530

    Update gemini_utils.py

--- gemini_utils.py
+++ gemini_utils.py
@@ -2,7 +2,7 @@ from google.generativeai import GenerativeModel
 import google.generativeai as genai
 
 # Paste your Gemini API key here
-genai.configure(api_key="AIzaSyD2q1ALHLR8BE4tx513Hwu-MHDO-BG7faw")
+genai.configure(api_key=st.secrets['gemini-key'])
 model = GenerativeModel("models/gemini-2.5-flash-preview-04-17")
 
 def query_gemini(prompt):
